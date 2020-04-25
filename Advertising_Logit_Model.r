1fea773471cf0635f6b0b5fc2e45551cc3970474
orangespriteman <hervinsagnep@gmail.com>
2020-04-25 02:05:30 -0700

    Update Advertising_Logit_Model.r

--- Advertising_Logit_Model.r
+++ Advertising_Logit_Model.r
@@ -1,3 +1,5 @@
+#To do: Chi-Square test, slope tests using Z-values and P-values
+
 #Importing data
 data <- read.csv('/Users/hervinsagnep/Fixed\ Advertising.csv')
 column_names <- c("Clicked.on.Ad","Daily.Time.Spent.on.Site","Area.Income")
@@ -23,16 +25,15 @@ prediction <- predict.glm(logit_model,test,type='response')
 test["Predictions"] <- prediction
 predict <- predict(logit_model,test,type='response')
 
-
 #Testing results
 test["Prediction"] <- ifelse(test["Predictions"] < .5,0,1)
 actual_frequency <- table(test$Clicked.on.Ad)
 predicted_frequency <- table(test$Prediction)
+test
 
 #Creating barplots for frequencies
 barplot(actual_frequency,xlab="Clicked on ad?",main="Actual Training Frequency",ylim=c(0,400),col=c("Red","Green"))
 barplot(predicted_frequency,xlab="Clicked on ad?",main="Predicted Training Frequency",ylim=c(0,400),col=c("Red","Green"))
-
 
 #McFadden's Pseudo R^2 and P-Value
 ll.null <- logit_model$null.deviance/-2
@@ -45,6 +46,4 @@ cat("P-Value:",p_value)
 #Values for testing slopes
 p_values <- coef(summary(logit_model))[,'Pr(>|z|)']
 z_values <- coef(summary(logit_model))[,'z value']
-
-
 
